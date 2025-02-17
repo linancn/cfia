2af27ad89045a55810bd192a5667f95436cafac6
jianchuanqi <qijianchuan@hotmail.com>
2025-02-17 23:24:49 +0800

    更新站点标题为“碳足迹产业技术创新联盟”

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: '碳足迹产业技术创新联盟',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
